db2f42806f360cf429a738805ab3e8b53321755c
Jun Gan <ganjun@microsoft.com>
2025-07-11 19:47:57 +0800

    improve the style of message bubble

--- YomiChat/frontend/src/components/Message.js
+++ YomiChat/frontend/src/components/Message.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-// import './Message.css'; // 可选：给气泡加点样式
 
 export default function Message({ from, avatar, content }) {
   const isUser = from === 'user';
